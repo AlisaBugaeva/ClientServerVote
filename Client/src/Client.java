373aafab572a5f3d6a9743b3aba9db0204696cae
AlisaBugaeva <alisabugaeva@mail.ru>
2023-03-23 18:37:40 +0300

    some improvements 3.0

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -53,91 +53,95 @@ public class Client {
 
         Scanner sc = new Scanner(System.in);
         String command = sc.nextLine();
-        String u ="";
-        String n;
-        String request ="";
+        String user ="";
+        String request;
         String response;
 
         while(!command.equals("exit")){
             if(command.equals("login")){
                 System.out.println("Enter username");
-                u = sc.nextLine();
-                System.out.println("You logged in as "+ u);
+                user = sc.nextLine();
+                System.out.println("You logged in as "+ user);
 
                 System.out.println("");
                 System.out.println(CHOOSING_COMMAND);
                 command= sc.nextLine();
             }
-            else if(!u.equals("")){
-                if(command.equals("create topic")){
-                    System.out.println("Enter the topic name");
-                    n=sc.nextLine();
-                    request = "create topic#" + n;
-                }
-                else if(command.equals("view")){
-                    request= "view";
-                }
-                else if(command.equals("create vote")){
-                    StringBuilder ans = new StringBuilder();
-                    ans.append("create vote#");
-                    System.out.println("Enter the topic name, in which you want to add a vote");
-                    ans.append(sc.nextLine()).append("#");
-                    ans.append(u).append("#");
-                    System.out.println("Enter the vote name");
-                    ans.append(sc.nextLine()).append("#");
-                    System.out.println("Enter the vote theme");
-                    ans.append(sc.nextLine()).append("#");
-                    System.out.println("Enter the number of answer options");
-                    String count = sc.nextLine();
-                    ans.append(count);
-                    System.out.println("Enter answer options using 'Enter'");
-                    for (int i = 0; i < Integer.parseInt(count); i++) {
-                        ans.append("#").append(sc.nextLine());
+            else if(!user.equals("")){
+                switch (command) {
+                    case "create topic" -> {
+                        System.out.println("Enter the topic name");
+                        String name = sc.nextLine();
+                        request = "create topic#" + name;
+                        break;
                     }
-                    request= String.valueOf(ans);
-                }
-                else if(command.equals("view vote")){
-                    System.out.println("Enter the topic name");
-                    String topic = sc.nextLine();
-                    System.out.println("Enter the vote name");
-                    String voteName = sc.nextLine();
-                    request = "view vote#" + topic+"#"+ voteName;
-
-                }
-                else if(command.equals("view votes")){
-                    System.out.println("Enter the topic name");
-                    String topic = sc.nextLine();
-                    request = "view votes#" + topic;
-
-                }
-                else if(command.equals("vote")){
-                    System.out.println("Enter the topic name");
-                    String topic = sc.nextLine();
-                    System.out.println("Enter the vote name");
-                    String voteName = sc.nextLine();
-                    request = "vote#" + topic+"#"+ voteName;
-                    writer.write(request);
-                    writer.newLine();
-                    writer.flush();
-                    response = reader.readLine();
-                    if(response.equals("There is no such vote, so we can't accept your vote")){
-                        request ="";
+                    case "view" -> {
+                        request = "view";
+                        break;
                     }
-                    else{
-                        System.out.println(response);
-                        request = sc.nextLine();
+                    case "create vote" -> {
+                        StringBuilder ans = new StringBuilder();
+                        ans.append("create vote#");
+                        System.out.println("Enter the topic name, in which you want to add a vote");
+                        ans.append(sc.nextLine()).append("#");
+                        ans.append(user).append("#");
+                        System.out.println("Enter the vote name");
+                        ans.append(sc.nextLine()).append("#");
+                        System.out.println("Enter the vote theme");
+                        ans.append(sc.nextLine()).append("#");
+                        System.out.println("Enter the number of answer options");
+                        String count = sc.nextLine();
+                        ans.append(count);
+                        System.out.println("Enter answer options using 'Enter'");
+                        for (int i = 0; i < Integer.parseInt(count); i++) {
+                            ans.append("#").append(sc.nextLine());
+                        }
+                        request = String.valueOf(ans);
                     }
-                }
-                else if(command.equals("delete")){
-                    System.out.println("Enter the topic name");
-                    String topic = sc.nextLine();
-                    System.out.println("Enter the vote name");
-                    String voteName = sc.nextLine();
-                    request = "delete#" + u + "#" + topic+"#"+ voteName;
-                }
-                else{
-                    System.out.println("You entered an invalid command");
-                    request="";
+                    case "view vote" -> {
+                        System.out.println("Enter the topic name");
+                        String topic = sc.nextLine();
+                        System.out.println("Enter the vote name");
+                        String voteName = sc.nextLine();
+                        request = "view vote#" + topic + "#" + voteName;
+                        break;
+                    }
+                    case "view votes" -> {
+                        System.out.println("Enter the topic name");
+                        String topic = sc.nextLine();
+                        request = "view votes#" + topic;
+                        break;
+                    }
+                    case "vote" -> {
+                        System.out.println("Enter the topic name");
+                        String topic = sc.nextLine();
+                        System.out.println("Enter the vote name");
+                        String voteName = sc.nextLine();
+                        request = "vote#" + topic + "#" + voteName;
+                        writer.write(request);
+                        writer.newLine();
+                        writer.flush();
+                        response = reader.readLine();
+                        if (response.equals("There is no such vote, so we can't accept your vote")) {
+                            request = "";
+                        } else {
+                            System.out.println(response);
+                            request = sc.nextLine();
+                        }
+                        break;
+                    }
+                    case "delete" -> {
+                        System.out.println("Enter the topic name");
+                        String topic = sc.nextLine();
+                        System.out.println("Enter the vote name");
+                        String voteName = sc.nextLine();
+                        request = "delete#" + user + "#" + topic + "#" + voteName;
+                        break;
+                    }
+                    default -> {
+                        System.out.println("You entered an invalid command");
+                        request = "";
+                    }
                 }
 
                 writer.write(request);
@@ -160,7 +164,7 @@ public class Client {
         writer.write(request);
         writer.newLine();
         writer.flush();
-        u="";
+        user="";
 
     }
 
@@ -168,72 +172,75 @@ public class Client {
 
         Scanner sc = new Scanner(new File(fileName));
         String command = sc.nextLine();
-        String u ="";
-        String n;
+        String user ="";
         String request ="";
         String response;
 
         while(!command.equals("exit")){
             if(command.equals("login")){
-                u = sc.nextLine();
+                user = sc.nextLine();
 
                 command= sc.nextLine();
             }
-            else if(!u.equals("")){
-                if(command.equals("create topic")){
-                    n=sc.nextLine();
-                    request = "create topic#" + n;
-                }
-                else if(command.equals("view")){
-                    request= "view";
-                }
-                else if(command.equals("create vote")){
-                    StringBuilder ans = new StringBuilder();
-                    ans.append("create vote#");
-                    ans.append(sc.nextLine()).append("#");
-                    ans.append(u).append("#");
-                    ans.append(sc.nextLine()).append("#");
-                    ans.append(sc.nextLine()).append("#");
-                    String count = sc.nextLine();
-                    ans.append(count);
-                    for (int i = 0; i < Integer.parseInt(count); i++) {
-                        ans.append("#").append(sc.nextLine());
+            else if(!user.equals("")){
+                switch (command) {
+                    case "create topic" -> {
+                        String name = sc.nextLine();
+                        request = "create topic#" + name;
+                        break;
                     }
-                    request= String.valueOf(ans);
-                }
-                else if(command.equals("view vote")){
-                    String topic = sc.nextLine();
-                    String voteName = sc.nextLine();
-                    request = "view vote#" + topic+"#"+ voteName;
-
-                }
-                else if(command.equals("view votes")){
-                    String topic = sc.nextLine();
-                    request = "view votes#" + topic;
-
-                }
-                else if(command.equals("vote")){
-                    String topic = sc.nextLine();
-                    String voteName = sc.nextLine();
-                    request = "vote#" + topic+"#"+ voteName;
-                    writer.write(request);
-                    writer.newLine();
-                    writer.flush();
-                    response = reader.readLine();
-                    if(response.equals("There is no such vote, so we can't accept your vote")){
-                        request ="";
+                    case "view" -> {
+                        request = "view";
+                        break;
                     }
-                    else{
-                        request = sc.nextLine();
+                    case "create vote" -> {
+                        StringBuilder ans = new StringBuilder();
+                        ans.append("create vote#");
+                        ans.append(sc.nextLine()).append("#");
+                        ans.append(user).append("#");
+                        ans.append(sc.nextLine()).append("#");
+                        ans.append(sc.nextLine()).append("#");
+                        String count = sc.nextLine();
+                        ans.append(count);
+                        for (int i = 0; i < Integer.parseInt(count); i++) {
+                            ans.append("#").append(sc.nextLine());
+                        }
+                        request = String.valueOf(ans);
+                        break;
                     }
-                }
-                else if(command.equals("delete")){
-                    String topic = sc.nextLine();
-                    String voteName = sc.nextLine();
-                    request = "delete#" + u + "#" + topic+"#"+ voteName;
-                }
-                else{
-                    request="wrong command";
+                    case "view vote" -> {
+                        String topic = sc.nextLine();
+                        String voteName = sc.nextLine();
+                        request = "view vote#" + topic + "#" + voteName;
+                        break;
+                    }
+                    case "view votes" -> {
+                        String topic = sc.nextLine();
+                        request = "view votes#" + topic;
+                        break;
+                    }
+                    case "vote" -> {
+                        String topic = sc.nextLine();
+                        String voteName = sc.nextLine();
+                        request = "vote#" + topic + "#" + voteName;
+                        writer.write(request);
+                        writer.newLine();
+                        writer.flush();
+                        response = reader.readLine();
+                        if (response.equals("There is no such vote, so we can't accept your vote")) {
+                            request = "";
+                        } else {
+                            request = sc.nextLine();
+                        }
+                        break;
+                    }
+                    case "delete" -> {
+                        String topic = sc.nextLine();
+                        String voteName = sc.nextLine();
+                        request = "delete#" + user + "#" + topic + "#" + voteName;
+                        break;
+                    }
+                    default -> request = "wrong command";
                 }
 
                 writer.write(request);
@@ -251,6 +258,6 @@ public class Client {
         writer.write(request);
         writer.newLine();
         writer.flush();
-        u="";
+        user="";
     }
 }
